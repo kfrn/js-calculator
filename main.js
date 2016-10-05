b570603762c0b9a33b9ef2e4b58703144ed78bb4
Katherine Nagels <knfrances@gmail.com>
2016-10-05 14:42:52 +1300

    Quick code cleanup

--- main.js
+++ main.js
@@ -1,22 +1,11 @@
 document.addEventListener('DOMContentLoaded', start);
 
 function start () {
-    // var equals = document.getElementById("equals");
-    // equals.addEventListener("click", calculateResult);
-
-    // var clear = document.getElementById("AC");
-    // clear.addEventListener("click", clearResult);
-
-    // var seven = document.getElementById("seven").value;
-    // console.log("seven is " + seven);
-
-
     var buttons = document.getElementsByTagName("button");
     // console.log(buttons);
     for (var i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener("click", getButton);
     }
-    // buttons.addEventListener("click", initializeButtons);
 }
 
 var equation = "";
@@ -36,8 +25,6 @@ function getButton(evt) {
     }
     // console.log(evt.target.value);
 }
-
-
 
 function calculateResult() {
     result = eval(equation);
