a339fd49e7c31560f8b245e245e057f0bd2442a1
Katherine Nagels <knfrances@gmail.com>
2016-10-08 09:56:08 +1300

    Tidy code

--- main.js
+++ main.js
@@ -1,20 +1,14 @@
-// TO DO LIST
-// 1. Find workaround for JS rounding issue
-// 2. Print error message if equation input is totally nonsensical
-
-
 document.addEventListener('DOMContentLoaded', start);
+
+var equation = "";
+var result = 0;
 
 function start () {
     var buttons = document.getElementsByTagName("button");
-    // console.log(buttons);
     for (var i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener("click", getButton);
     }
 }
-
-var equation = "";
-var result = 0;
 
 function getButton(evt) {
     if (evt.target.id === "AC") {
@@ -28,7 +22,6 @@ function getButton(evt) {
         console.log(equation);
         document.getElementById("equation").innerHTML = "<p>" + equation + "</p>";
     }
-    // console.log(evt.target.value);
 }
 
 function calculateResult() {
@@ -62,28 +55,12 @@ function clearLast() {
   var lastDigit = equation[equation.length - 1];
   if (lastDigit === "+" || lastDigit === "-" || lastDigit === "*" || lastDigit === "/") {
     equation = equation.slice(0, equation.length -1);
-    // document.getElementById("equation").innerHTML = "<p>" + equation + "</p>";
   } else {
     var operators = /[\/\+\-\*]/g; // To match all arithmetic operators
     var tempArray = equation.split(operators);
     var lastElementLength = tempArray[tempArray.length -1].length;
     equation = equation.slice(0, equation.length - lastElementLength);
   }
-  // document.getElementById("equation").innerHTML = "<p>" + equation + "</p>";
-}
-
-function clearLast() {
-  var lastDigit = equation[equation.length - 1];
-  if (lastDigit === "+" || lastDigit === "-" || lastDigit === "*" || lastDigit === "/") {
-    equation = equation.slice(0, equation.length -1);
-    // document.getElementById("equation").innerHTML = "<p>" + equation + "</p>";
-  } else {
-    var operators = /[\/\+\-\*]/g; // To match all arithmetic operators
-    var tempArray = equation.split(operators);
-    var lastElementLength = tempArray[tempArray.length -1].length;
-    equation = equation.slice(0, equation.length - lastElementLength);
-  }
-  // document.getElementById("equation").innerHTML = "<p>" + equation + "</p>";
   isEmpty();
 }
 
